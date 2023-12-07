04cac40c5b0b73427fb75242331e33854b4cb105
Daniel Alves <117676329+Danielalves33147@users.noreply.github.com>
2023-12-07 18:10:34 -0300

    Add files via upload

---
+++ estoque/src/main/java/com/ifba/estoque/ItemModel.java
@@ -0,0 +1,56 @@
+package com.ifba.estoque;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "itens")
+
+public class ItemModel {
+    
+    @Id
+@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "itens_generator")
+@SequenceGenerator(name = "itens_generator", sequenceName = "public.itens_seq", allocationSize = 1)
+Long id;
+    
+    String nomeItem;
+    int quantidadeItem;
+
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+    public void setNomeItem(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    public int getQuantidadeItem() {
+        return quantidadeItem;
+    }
+
+    public void setQuantidadeItem(int quantidadeItem) {
+        this.quantidadeItem = quantidadeItem;
+    }
+    
+            
+}
